254ccfd7981926037f4a07cbb86e92745cd32f1d
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-03-08 21:42:38 +0700

    Update

--- backend/pb_hooks/upload.pb.js
+++ backend/pb_hooks/upload.pb.js
@@ -1,39 +1,30 @@
-// The new hook name is "onRecordCreate" or "onRecordCreateRequest"
-// onRecordCreate((e) => {
-//   e.next();
-//   try {
-//     console.log("Processing file for record:", e.record.id);
-//     e.record.set("is_eval_class", true);
-//     $app.save(e.record);
-//     console.log("Hook completed successfully.");
-//   } catch (err) {
-//     console.error("Hook failed:", err);
-//   }
-// }, "upload");
-
+// This hook runs before a record is created in the "uploads" collection
+// The file is not yet saved to storage at this point, so we cannot access the file URL or path
 onRecordCreateRequest((e) => {
-  console.log("Processing file for record...");
-
   try {
-    // console.log(
-    //   "Original record data:",
-    //   e.record.id ?? "new record",
-    //   e.record.get("is_eval_class"),
-    // );
-    // 1. Modify the record BEFORE it hits the database
-    // e.record.set("is_eval_class", true);
+    //  Automatically set the "user" field to the current user's ID
     if (e.auth) {
-      // 2. Automatically set the "user" field to the current user's ID
       e.record.set("user", e.auth.id);
     }
-    // console.log(
-    //   "Modified record data:",
-    //   e.record.id ?? "new record",
-    //   e.record.get("is_eval_class"),
-    // );
-    // 2. e.next() handles the actual saving of the modified record
   } catch (err) {
     console.error("Error in onRecordCreateRequest hook:", err);
+  }
+  return e.next();
+}, "uploads");
+
+// This hook runs after a record is successfully created in the "uploads" collection
+// We can now access the record ID and collection ID and file name, but the file may not yet be fully processed and available in storage
+onRecordAfterCreateSuccess((e) => {
+  try {
+    const collectionId = e.record.collection().id;
+    const recordId = e.record.id;
+    const filename = e.record.get("image");
+    if (filename) {
+      const fullUrl = `storage/${collectionId}/${recordId}/${filename}`;
+      console.error("Full URL:", fullUrl);
+    }
+  } catch (err) {
+    console.error("Error in onRecordAfterCreateSuccess hook:", err);
   }
   return e.next();
 }, "uploads");
